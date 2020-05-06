baa3a0e232b5457218794f0e1e34b0544c3c70b7
Guilherme Nigri <guilherme.nigri@al.infnet.edu.br>
2020-05-06 08:02:14 -0300

    complementando codigo do startup

--- RedeSocial.Apresentacao/Startup.cs
+++ RedeSocial.Apresentacao/Startup.cs
@@ -26,8 +26,8 @@ namespace RedeSocial.Apresentacao
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +36,7 @@ namespace RedeSocial.Apresentacao
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
